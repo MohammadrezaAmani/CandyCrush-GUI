725fbf6675f818cc670eb82d775dde8a40f49db7
Mohammadreza Amani <64687226+MohammadrezaAmani@users.noreply.github.com>
2022-02-10 19:22:50 +0330

    Add HomePage.java

--- master/HomePage.java
+++ master/HomePage.java
@@ -78,56 +78,6 @@ public class HomePage extends JFrame implements ActionListener {
         panel.add(buttonPanel);
         frame.add(panel);
         frame.setVisible(true);
-
-        // setSize(500, 500);
-        // setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // setLayout(new BorderLayout());
-        // panel = new JPanel();
-        // panel.setLayout(new GridLayout(2, 1));
-        // label = new JLabel("Welcome to Candy Crush");
-        // label.setFont(new Font("Serif", Font.BOLD, 40));
-        // label.setHorizontalAlignment(JLabel.CENTER);
-        // labelPanel = new JPanel();
-        // labelPanel.setLayout(new GridLayout(1, 1));
-        // labelPanel.add(label);
-        // panel.add(labelPanel);
-        // buttonPanel = new JPanel();
-        // buttonPanel.setLayout(new GridLayout(4, 1));
-        // start = new JButton("Start New Game");
-        // start.addActionListener(this);
-        // load = new JButton("Load Game");
-        // load.addActionListener(this);
-        // about = new JButton("About");
-        // about.addActionListener(this);
-        // exit = new JButton("Exit");
-        // exit.addActionListener(this);
-        // Highscores1 = new JButton("Highscores");
-        // //design the buttons to look like candy crush
-        // start.setBackground(Color.red);
-        // start.setForeground(Color.white);
-        // start.setFont(new Font("Serif", Font.BOLD, 20));
-        // load.setBackground(Color.red);
-        // load.setForeground(Color.white);
-        // load.setFont(new Font("Serif", Font.BOLD, 20));
-        // about.setBackground(Color.red);
-        // about.setForeground(Color.white);
-        // about.setFont(new Font("Serif", Font.BOLD, 20));
-        // exit.setBackground(Color.red);
-        // exit.setForeground(Color.white);
-        // exit.setFont(new Font("Serif", Font.BOLD, 20));
-        // Highscores1.setBackground(Color.red);
-        // Highscores1.setForeground(Color.white);
-        // Highscores1.setFont(new Font("Serif", Font.BOLD, 20));
-        // Highscores1.addActionListener(this);
-
-        // buttonPanel.add(start);
-        // buttonPanel.add(load);
-        // buttonPanel.add(about);
-        // buttonPanel.add(exit);
-        // buttonPanel.add(Highscores1);
-        // panel.add(buttonPanel);
-        // add(panel, BorderLayout.CENTER);
-        // setVisible(true);
     }
 
     public void actionPerformed(ActionEvent e) {
